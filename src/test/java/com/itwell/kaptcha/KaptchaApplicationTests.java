b86b292049462829ea8f69803ed30404fe1b608c
itwell <822210260@qq.com>
2021-03-21 01:45:34 +0800

    springboot整合kaptcha

---
+++ src/test/java/com/itwell/kaptcha/KaptchaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itwell.kaptcha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KaptchaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
